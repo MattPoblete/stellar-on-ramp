777acffe76850eff9a8e21cc48893091f0a90e4e
Matias Poblete <matias.pobleteduran@gmail.com>
2024-05-24 06:11:50 -0400

    ✨Add helpers & methods for SEP-24 deposit

---
+++ src/methods/sep24Deposit/index.ts
@@ -0,0 +1,11 @@
+import { checkInfo } from "./checkInfo";
+import { interactiveDepositFlow } from "./interactiveDepositFlow";
+import { createPopup } from "./createPopup";
+import { pollDepositUntilComplete } from "./pollDepositUntilComplete";
+
+export {
+  checkInfo,
+  interactiveDepositFlow,
+  createPopup,
+  pollDepositUntilComplete,
+};
